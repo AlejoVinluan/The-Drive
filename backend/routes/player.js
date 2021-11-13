67afee9eb8d06151af76b357287414c340878fba
AlejoVinluan <alejo.vinluan146@gmail.com>
2021-11-13 15:20:01 -0600

    Added player routing
    Title

--- backend/routes/player.js
+++ backend/routes/player.js
@@ -1,3 +1,10 @@
 const router = require('express').Router()
 let Player = require('../models/player.model')
 
+router.route('/').get((req, res) => {
+    Player.find()
+    .then(players => res.json(players))
+    .catch(err => res.status(400).json('Error: ' + err))
+})
+
+module.exports = router
